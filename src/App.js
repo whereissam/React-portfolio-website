698064491c9fbb0241890c531e5c35a0d4c2f37c
Sam <63341022+whereissam@users.noreply.github.com>
2022-04-05 17:15:33 +0800

    Finish text animation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.scss';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './compomemts/Layout';
+import Home from './compomemts/Home';
 
 function App() {
   return (
     <>
     <Routes>
-      <Route path='/' element={<Layout />}></Route>
+      <Route path='/' element={<Layout />}>
+      <Route index element={<Home></Home>}></Route>
+      </Route>
+      
     </Routes>
     </>
   );
